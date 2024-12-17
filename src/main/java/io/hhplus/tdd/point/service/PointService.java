5d4979dfe93ee50807929846536d2f9d960f573f
CHOHYUNHWA <gusghk115@gmail.com>
2024-12-17 09:52:57 +0900

    feat: 서비스 메서드 구조 생성 및 컨트롤러 적용 - 비즈니스로직 미 구현 - 컨트롤러 코드 적용

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -1,11 +1,37 @@
 package io.hhplus.tdd.point.service;
 
+import io.hhplus.tdd.point.domain.PointHistory;
+import io.hhplus.tdd.point.domain.UserPoint;
+import io.hhplus.tdd.point.repository.PointHistoryRepository;
+import io.hhplus.tdd.point.repository.UserPointRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class PointService {
+
+    private final UserPointRepository userPointRepository;
+    private final PointHistoryRepository pointHistoryRepository;
+
+    public UserPoint getPoint(long userId){
+        return null;
+    }
+
+    public UserPoint charge(long userId, long amount){
+        return null;
+    }
+
+    public UserPoint use(long userId, long amount) {
+        return null;
+    }
+
+    public List<PointHistory> getAllHistory(long userId){
+        return null;
+    }
+
 }
